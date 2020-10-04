f846b1edce35da9317ede01e6286a19b7f70b9fb
Slava <slavaknyazev2003@gmail.com>
2020-10-04 19:55:53 +0600

    сделал не большие изменения

--- src/model.js
+++ src/model.js
@@ -11,21 +11,10 @@ export const model = [
             padding: '1.5rem'
         }
     }),
-    new ImageBlock( image, {
-        styles: {
-            padding: '2rem 0',
-            display: 'flex',
-            'justify-content': 'center'
-        },
-        imageStyles: {
-            width: '500px',
-            height: 'auto'
-        }
-    }),
     new ColumnsBlock( [
         'Приложение на чистом JavaScript без использования библиотек',
-        'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
-        'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
+        'В веб-приложении использовались SOLID и ООП',
+        'JavaScript - это просто, и интересно'
     ], {
         styles: {
             background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
@@ -34,7 +23,7 @@ export const model = [
             'font-weight': 'bold'
         }
     }),
-    new TextBlock( 'Это моё крутое веб-приложение, которое я создал за 2 дня', {
+    new TextBlock( 'Это веб-приложение я создал за 2 дня. Использовал я чистый JavaScript, где применил SOLID, а так же ООП. С помощью Parcel я сделал проект удобным, а так же написал читаемый и красивый код.', {
         styles: {
             background: 'linear-gradient(to left, #f2994a, #f2c94c)',
             padding: '1rem',
